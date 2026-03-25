58b9354bc7646414755d31d3a6c21ed86f0f698d
dominowit <khopeshnicc@gmail.com>
2026-03-25 12:51:06 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -610,8 +610,7 @@ export default function GlowaDoksef() {
         const { done, value } = await reader.read();
         if (done) break;
         buffer += decoder.decode(value, { stream: true });
-        const lines = buffer.split("
-");
+        const lines = buffer.split("\n");
         buffer = lines.pop();
 
         for (const line of lines) {
